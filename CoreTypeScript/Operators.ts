1d0939406f61d07e9417c466bba1a4874ef1eb80
learnautomation1604-cyber <learnautomation1604@gmail.com>
2026-06-21 18:13:07 +0530

    OperatorNotes

--- CoreTypeScript/Operators.ts
+++ CoreTypeScript/Operators.ts
@@ -1,10 +1,93 @@
 // Types Of Operaters - Arithmetic Operator, Assignmenet Operators, Comperison Operators, Logical Operator, Increment/Decrment Operator, Ternery Operators, Type Operator, 
 
-//1. Arithmetic Operator
+// 1. Arithmetic Operator - Addition , Subraction, Mutiplication, Division, Mosules, Exponentials
 
-var x =10;
-var y = 20;
+// var a:number = 10;
+// var b:number = 30;
 
-var z=x+y;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
 
-console.log(z)
+//2. Assignment Operator 
+
+// var x:number = 10;
+// // var x = x+5;///
+// x += 5 ;//addition assignment Operator - x=x+5
+// console.log(x)
+// x -=3 ; // Subtraction Assignment Operator
+// console.log(x)
+
+// Mutiplication Assignment Operator
+// Division Assignment Operator
+// Remainder Assignment Operator %
+// Exponential Assignment Operator **
+// Left shift Assignment Operator
+// Right Shift Assignment Operator
+// UnAssigned Right Shift Assignment Operator
+// Bitwise AND Assignment Operator
+// BitWise OR Assignment Operator
+// Bitwise XOR Assignment Operator
+// Logical AND Assignment Operator
+// Logical OR Assignment Operator
+
+// 3. Comparison Operator 
+
+// == ---- Equal (Loose)
+
+// var x:number = 10;
+// var y:string = "10";
+
+
+// console.log(x == y);
+// console.log(x === y);
+
+
+// === --- Strict Equal
+// != --- Not Equal
+// !== --- Strict Not Equal
+// > --- Greater Than
+// < ---Less than
+// >= --- Greater than or equal
+// <= --- Less than or equal
+
+
+// Logical Operator - Logical AND, Logical OR, Logical NOT
+
+// Logical AND 
+
+// var age = 30;
+// var haslicence = true;
+
+// if(age >= 18 && haslicence){
+//     console.log("Car Drive")
+// }else{
+//     console.log("Canot run a car")
+// }
+
+
+// Logical OR
+
+// var isstudent = false;
+// var isEmployee = false;
+
+// if(isstudent || isEmployee){
+//     console.log("Discount available")
+
+// }else{
+//     console.log("Discuount not available")
+// }
+
+// Logical NOT
+
+var isloggin = true;
+
+if(!isloggin){
+    console.log("Please login")
+
+}else{
+    console.log("you are already logged in")
+}
